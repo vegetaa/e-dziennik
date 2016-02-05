a62bfd49dabbeae99a499bf5f1e575c6037e52c3
vegekox <kizok4000@wp.pl>
2016-02-05 21:21:42 +0100

    Create dodaj_admina.php

---
+++ dodaj_admina.php
@@ -0,0 +1,28 @@
+<!DOCTYPE HTML>
+<html lang="pl">
+<head>
+<meta charset="utf-8">
+<meta name="Author" content="Tomasz Kozik, Joanna Stefan, Sԡwomir Morawskis">
+<title>E-DZIENNIK</title>
+</head>
+<body style="text-align:center; background-color: #E8820C;">
+<h1>DODAJ ADMINISTRATORA</h1>
+<form action="dodaj_admina_doo.php" method="post">
+<p>LOGIN</p></br>
+<input type="text" name="login"/></br>
+<p>HASłO</p>
+<input type="password" name="password"/></br>
+<p>POTWIERDŹ HASŁO</p>
+<input type="password" name="password2"/></br>
+<p>IMIĘ</p>
+<input type="text" name="name"/></br>
+<p>NAZWISKO</p>
+<input type="text" name="surname"/></br>
+<p>E-MAIL</p>
+<input type="text" name="email"/></br>
+<p>TELEFON</p>
+<input type="text" name="phone"/></br>
+<input type="submit" value="Wyślij"/>
+</form>
+</body>
+</html>
