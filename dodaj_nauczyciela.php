9da4ffc0088af1833b48bc78f16e554ef4d25907
vegekox <kizok4000@wp.pl>
2016-02-05 21:24:49 +0100

    Create dodaj_nauczyciela.php

---
+++ dodaj_nauczyciela.php
@@ -0,0 +1,30 @@
+<!DOCTYPE HTML>
+<html lang="pl">
+<head>
+<meta charset="utf-8">
+<meta name="Author" content="Tomasz Kozik, Joanna Stefan, S?womir Morawskis">
+<title>E-DZIENNIK</title>
+</head>
+<body style="text-align:center; background-color: #E8820C;">
+<h1>DODAJ UCZNIA</h1>
+<form action="dodaj_nauczyciela_doo.php" method="post">
+<p>LOGIN</p></br>
+<input type="text" name="login"/></br>
+<p>HASŁO</p>
+<input type="password" name="password"/></br>
+<p>POTWIERDŹ HASŁO</p>
+<input type="password" name="password2"/></br>
+<p>IMIĘ</p>
+<input type="text" name="name"/></br>
+<p>NAZWISKO</p>
+<input type="text" name="surname"/></br>
+<p>E-MAIL</p>
+<input type="text" name="email"/></br>
+<p>TELEFON</p>
+<input type="text" name="phone"/></br>
+<p>PRZEDMIOT</p>
+<input type="text" name="przedmiot"/></br>
+<input type="submit" value="Wyślij"/>
+</form>
+</body>
+</html>
