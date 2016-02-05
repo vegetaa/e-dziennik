e67cf1c18d98b48dac0f2da663eb57553ea62dd4
vegekox <kizok4000@wp.pl>
2016-02-05 21:19:44 +0100

    Create admin.php

---
+++ admin.php
@@ -0,0 +1,18 @@
+<!DOCTYPE HTML>
+<html lang="pl">
+<head>
+<meta charset="utf-8">
+<meta name="Author" content="Tomasz Kozik, Joanna Stefan, Sԡwomir Morawskis">
+<title>E-DZIENNIK</title>
+</head>
+<body style="text-align:center; background-color: #E8820C;">
+<h1>ZALOGUJ SIĘ JAKO ADMINISTRATOR</h1>
+<form action="login_admin.php" method="post">
+<p>LOGIN</p></br>
+<input type="text" name="login"/></br>
+<p>HASłO</p></br>
+<input type="password" name="password"/></br>
+<input type="submit" value="Wyślij"/>
+</form>
+</body>
+</html>
