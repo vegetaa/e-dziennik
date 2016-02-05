41ccc48b4ab19f77dbb857378d29e6124e910107
vegekox <kizok4000@wp.pl>
2016-02-05 21:29:59 +0100

    Create nauczyciel_panel.php

---
+++ nauczyciel_panel.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+<html lang="pl">
+<head>
+<meta charset="utf-8">
+<meta name="Author" content="Tomasz Kozik, Joanna Stefan, Sԡwomir Morawskis">
+<title>E-DZIENNIK</title>
+</head>
+<body style="text-align:center; background-color: #E8820C;">
+<h1>PANEL NAUCZYCIELA</h1>
+<p><?php echo $output; ?></p>
+<a href="dodaj_ocene.php">DODAJ OCENE</a><br>
+<a href="edytuj_ocene.php">EDYTUJ OCENE</a><br>
+</body>
+</html>
