eb0090f2d80702151bdf4d8ebeb00e67f5ea4101
vegekox <kizok4000@wp.pl>
2016-02-05 21:23:44 +0100

    Create dodaj_klase.php

---
+++ dodaj_klase.php
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML>
+<html lang="pl">
+<head>
+<meta charset="utf-8">
+<meta name="Author" content="Tomasz Kozik, Joanna Stefan">
+<title>E-DZIENNIK</title>
+</head>
+<body style="text-align:center; background-color: #E8820C;">
+<h1>DODAJ KLASĘ</h1>
+<form action="dodaj_klase_doo.php" method="post">
+<p>KLASA</p></br>
+<input type="text" name="klasa"/></br>
+<input type="submit" value="Wyślij"/>
+</form>
+</body>
+</html>
