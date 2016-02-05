80f05f82979c5e2f08e3378c1d5dd51d11a75c9d
vegekox <kizok4000@wp.pl>
2016-02-05 21:27:37 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE HTML>
+<html lang="pl">
+<head>
+<meta charset="utf-8">
+<meta name="Author" content="Tomasz Kozik, Joanna Stefan, Sԡwomir Morawskis">
+<title>E-DZIENNIK</title>
+</head>
+<body style="text-align:center; background-color: #E8820C;">
+<h1>E-DZIENNIK</h1>
+<p><?php echo $output; ?></p>
+<a href="admin.php">ADMINISTRATOR</a><br>
+<a href="uczen.php">UCZEŃ</a><br>
+<a href="nauczyciel.php">NAUCZYCIEL</a>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<p> Niemasz konta jeszcze? Napisz!</p>
+<a href="mailto:tkozik@e-dzinnik.pl">TUTAJ</a>
+
+</body>
+</html>
