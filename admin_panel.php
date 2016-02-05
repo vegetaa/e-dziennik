e18c4e143930815debcf71bb66b31812824991a5
vegekox <kizok4000@wp.pl>
2016-02-05 21:20:20 +0100

    Create admin_panel.php

---
+++ admin_panel.php
@@ -0,0 +1,18 @@
+<!DOCTYPE HTML>
+<html lang="pl">
+<head>
+<meta charset="utf-8">
+<meta name="Author" content="Tomasz Kozik, Joanna Stefan, Sԡwomir Morawskis">
+<title>E-DZIENNIK</title>
+</head>
+<body style="text-align:center; background-color: #E8820C;">
+<h1>PANEL ADMINISTRATORA</h1>
+<p><?php echo $output; ?></p>
+<a href="dodaj_admina.php">DODAJ ADMINISTRATORA</a><br>
+<a href="dodaj_ucznia.php">DODAJ UCZNIA</a><br>
+<a href="dodaj_nauczyciela.php">DODAJ NAUCZYCIELA</a><br>
+<a href="dodaj_klase.php">DODAJ KLASE</a><br>
+<a href="przydziel_klase.php">PRZYDZIEL KLASE</a><br>
+<a href="usun_ucznia.php">USUŃ UCZNIA</a>
+</body>
+</html>
